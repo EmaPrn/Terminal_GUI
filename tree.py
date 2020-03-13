0285275949728ac302095bc542e3dbc9a0b8f6be
emanuele <52465428+EmaPrn@users.noreply.github.com>
2020-03-14 00:02:15 +0100

    Types hints added for tree module.

--- tree.py
+++ tree.py
@@ -1,4 +1,61 @@
-def _node_iterator(node):
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+from typing import Any, Union, List
+
+
+class Node(object):
+    def __init__(self, name: str, payload: Any = None):
+        self._name: str = name
+        self._payload: Any = payload
+        self._parent: Union[Node, None] = None
+        self._children: List[Node] = []
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def payload(self) -> Any:
+        return self._payload
+
+    @property
+    def parent(self) -> Node:
+        return self._parent
+
+    @parent.setter
+    def parent(self, parent: Node) -> None:
+        if self._parent:
+            self._parent.remove_child(self)
+        self._parent = parent
+
+    def has_children(self) -> bool:
+        return True if len(self._children) > 0 else False
+
+    def add_child(self, child: Node):
+        if child not in self._children:
+            self._children.append(child)
+            child.parent = self
+
+    def remove_child(self, child: Node):
+        if child in self._children:
+            self._children.remove(child)
+
+    def get_child(self, child_name: str) -> Node:
+        for child in self._children:
+            if child.name == child_name:
+                return child
+
+    def __str__(self):
+        return self._name
+
+    def __iter__(self) -> Node:
+        for child in self._children:
+            yield child
+
+
+def _node_iterator(node: Node) -> Union[Node, _node_iterator]:
     if not node.has_children():
         yield node
     else:
@@ -12,92 +69,41 @@ def _node_iterator(node):
                     stop = True
 
 
-class Node:
-    def __init__(self, name, payload=None):
-        self._name = name
-        self._payload = payload
-        self._parent = None
-        self._children = []
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def payload(self):
-        return self._payload
-
-    @property
-    def parent(self):
-        return self._parent
-
-    @parent.setter
-    def parent(self, parent):
-        if self._parent:
-            self._parent.remove_child(self)
-        self._parent = parent
-
-    def has_children(self):
-        return True if len(self._children) > 0 else False
-
-    #TODO: Handle type of child and errors
-    def add_child(self, child):
-        if child not in self._children:
-            self._children.append(child)
-            child.parent = self
-
-    def remove_child(self, child):
-        if child in self._children:
-            self._children.remove(child)
-
-    def get_child(self, child_name):
-        for child in self._children:
-            if child.name == child_name:
-                return child
-
-    def __str__(self):
-        return self._name
-
-    def __iter__(self):
-        for child in self._children:
-            yield child
-
-
-class Tree:
-    def __init__(self, root="root", root_payload=None):
+class Tree(object):
+    def __init__(self, root: Union[str, Node] = "root", root_payload: Any = None):
         if isinstance(root, str):
-            self._root = Node(root, root_payload)
+            self._root: Node = Node(root, root_payload)
         elif isinstance(root, Node):
-            self._root = root
-        self._current = self._root
-        self._iterator = _node_iterator(self._root)
+            self._root: Node = root
+        self._current: Node = self._root
+        self._iterator: _node_iterator = _node_iterator(self._root)
 
     @property
-    def root(self):
+    def root(self) -> Node:
         return self._root
 
     @property
-    def current(self):
+    def current(self) -> Node:
         return self._current
 
-    def get_node(self, name, node=None):
+    def get_node(self, name: str, node: Node = None) -> Union[None, Node]:
         if not node:
-            node = self._root
+            node: Node = self._root
 
         if node.name == name:
             return node
         else:
             for child in node:
-                found = self.get_node(name, child)
+                found: Node = self.get_node(name, child)
                 if found:
                     return found
         return None
 
-    def _reset_current(self):
+    def _reset_current(self) -> None:
         self._iterator = _node_iterator(self._root)
         self._current = next(self._iterator)
 
-    def set_next(self):
+    def set_next(self) -> Node:
         try:
             self._current = next(self._iterator)
         except StopIteration:
